5ac5cb6ac3d4f1a45dd73d80dd6b0a1dbd7e20ea
helciodev <helcioandre@live.com>
2021-05-21 11:12:16 +0100

    Add content for dish and headingOneMenu elements

--- modules/menu.js
+++ modules/menu.js
@@ -9,3 +9,15 @@ const dish2 = document.createElement('div');
 addClasses(headingOneMenu, 'text-3xl gray-500 font-bold opacity-100')
 addClasses(menuContainer, 'mx-auto w-1/2 bg-white opacity-80 px-5 py-3 text-center')
 [dish, dish2].forEach(dish => addClasses(dish, 'mt-4 grid grid-cols-2'))
+
+dish.insertAdjacentHTML('beforeend' `
+<div class="rounded-sm bg-blue-700">
+<img src="${Lasagna}" class="h-auto w-full" alt="">
+</div>
+<p class="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum pariatur aspernatur aut omnis nemo. Laboriosam rem beatae nulla enim saepe, vel modi amet?</p>
+`)
+
+headingOneMenu.textContent = "Gary's menu";
+
+appendChildren(menuContainer, headingOneMenu, dish)
+export{ menuContainer, dish }
