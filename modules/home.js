962eaa26de6b12130d3a02a78944c84ded45423f
helciodev <helcioandre@live.com>
2021-05-21 10:58:03 +0100

    Add classes to menu elements

--- modules/home.js
+++ modules/home.js
@@ -7,3 +7,17 @@ function appendChildren(node,...children) {
     node.appendChild(child)
   })
 }
+
+const home = document.createElement('div');
+const headingOne = document.createElement('h1');
+const paragraph = document.createElement('p');
+const headingTwo = document.createElement('h2');
+const horaryContainer = document.createElement('div')
+const location = document.createElement('div')
+
+addClasses(paragraph, 'mt-4 text-opacity-100')
+addClasses(headingOne,'text-3xl gray-500 font-bold opacity-100')
+addClasses(headingTwo, 'text-2xl mt-4 font-bold');
+addClasses(home,'mx-auto w-1/2 bg-white opacity-80 px-5 py-3 text-center' )
+addClasses(horaryContainer,'mt-4 flex justify-center' );
+location.classList.add('mt-4');
