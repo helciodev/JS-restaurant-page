343ca57816851f13d759b6533cc0a5fa76edf6bc
helciodev <helcioandre@live.com>
2021-05-21 11:10:25 +0100

    Add classes for menu elements

--- modules/menu.js
+++ modules/menu.js
@@ -5,3 +5,7 @@ const menuContainer = document.createElement('div');
 const headingOneMenu = document.createElement('h1');
 const dish = document.createElement('div');
 const dish2 = document.createElement('div');
+
+addClasses(headingOneMenu, 'text-3xl gray-500 font-bold opacity-100')
+addClasses(menuContainer, 'mx-auto w-1/2 bg-white opacity-80 px-5 py-3 text-center')
+[dish, dish2].forEach(dish => addClasses(dish, 'mt-4 grid grid-cols-2'))
