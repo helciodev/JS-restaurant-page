c892f846d7fee3364b8b6b7219130a383a5a3751
helciodev <helcioandre@live.com>
2021-05-21 10:44:31 +0100

    Add navigation as an adjacentHTML to the container, add module folder and home.js , menu.js and contact.js files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Iam  coming from src/index.js file')
+import { home } from '../modules/home.js';
+import { menuContainer, dish } from '../modules/menu.js';
+import { iamContact } from '../modules/contact.js';
+
+const content = document.getElementById('content');
+
+content.insertAdjacentHTML('beforeend', 
+`<nav class="bg-black pt-5 pb-4 mb-10">
+      <ul class="flex justify-center">
+        <li class="mr-10"><a href="#" id="home"  class="text-gray-100 capitalize text-2xl  pb-2 hover:text-red-500">home</a></li>
+        <li class="mr-10"><a href="#" id="menu" class="text-gray-100 capitalize text-2xl hover:text-red-500">menu</a></li>
+        <li><a href="#" id="contact" class="text-gray-100 capitalize text-2xl  hover:text-red-500">contact</a></li>
+      </ul>
+    </nav>`);
+   console.log(dish)
+   console.log('hello, world')
+    content.appendChild(menuContainer) ;
