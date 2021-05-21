b0783790f01ea58b0165f85005a616cb5e8f1fea
helciodev <helcioandre@live.com>
2021-05-21 10:55:47 +0100

    Create function appendChildren inside home.js file

--- modules/home.js
+++ modules/home.js
@@ -1,3 +1,9 @@
 function addClasses(elem, classes) {
   classes.split(' ').forEach(clas => elem.classList.add(clas))
 }
+
+function appendChildren(node,...children) {
+  children.forEach( function(child){
+    node.appendChild(child)
+  })
+}
