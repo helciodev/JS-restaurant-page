1f6762755943c8e84a8ad412d0b6cda17de69245
helciodev <helcioandre@live.com>
2021-05-21 11:09:25 +0100

    Import functions from home module to menu module and create some HTML elements for the menu

---
+++ modules/menu.js
@@ -0,0 +1,7 @@
+import {appendChildren, addClasses} from './home';
+import Lasagna from '../assets/images/lasagna.jpg';
+
+const menuContainer = document.createElement('div');
+const headingOneMenu = document.createElement('h1');
+const dish = document.createElement('div');
+const dish2 = document.createElement('div');
